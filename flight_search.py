3d18f2babb314d34b02b813dbc7ab75acd03bd1e
danspinelli <84397398+danspinelli@users.noreply.github.com>
2021-06-22 16:30:31 -0400

    hello world update

---
+++ flight_search.py
@@ -0,0 +1,16 @@
+#import requests
+#
+#url = "https://skyscanner-skyscanner-flight-search-v1.p.rapidapi.com/apiservices/browsedates/v1.0/US/USD/en-US/SFO-sky/LAX-sky/2021-06-22"
+#
+#querystring = {"inboundpartialdate":"2021-06-23"}
+#
+#headers = {
+#    'x-rapidapi-key': "e5eb6fe9efmshdc31df18adcf9acp183317jsndafb7aac79e2",
+#    'x-rapidapi-host': "skyscanner-skyscanner-flight-search-v1.p.rapidapi.com"
+#    }
+#
+#response = requests.request("GET", url, headers=headers, params=querystring)
+#
+#print(response.text)
+
+print("Hello World")
